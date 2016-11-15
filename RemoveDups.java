9c6c5928ba41eb65f749e9b8fcb949ba30d1236c
Al Miller <al.miller@ajmiller.net>
2016-11-15 13:24:19 -0800

    add main class

---
+++ RemoveDups.java
@@ -0,0 +1,18 @@
+public class RemoveDups {
+
+
+
+    public static void main(String [] args) {
+
+        if (args.length != 1) {
+
+            System.err.println("Usage: RemoveDups <input_filename>");
+            return;
+        }
+
+        
+
+    }
+
+
+}
